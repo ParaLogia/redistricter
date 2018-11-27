29242271c80b62ff67b95fdff6c4966cb5d4a089
Phillip Huang <phillip.huang@stonybrook.edu>
2018-11-26 20:23:15 -0500

    Initialize weights map in ObjectiveFunctionBuilder

--- redistricter/src/main/java/giants/redistricter/algorithm/ObjectiveFunctionBuilder.java
+++ redistricter/src/main/java/giants/redistricter/algorithm/ObjectiveFunctionBuilder.java
@@ -1,15 +1,17 @@
 package giants.redistricter.algorithm;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ObjectiveFunctionBuilder {
-    Map<ObjectiveCriteria,Double> weights;
+    Map<ObjectiveCriteria,Double> weights = new HashMap<>();
     ObjectiveFunction objFct;
 
     public ObjectiveFunctionBuilder addWeight(ObjectiveCriteria obj, Double weight){
         weights.put(obj,weight);
         return this;
     }
+
     public ObjectiveFunction build(){
         if (weights.size() > 0){
             objFct = new ObjectiveFunction(weights);
