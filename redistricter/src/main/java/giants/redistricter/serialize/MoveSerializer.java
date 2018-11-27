07bec26785f3039d588174108c6ad9d5defc4f22
Phillip Huang <phillip.huang@stonybrook.edu>
2018-11-27 00:20:22 -0500

    Add custom JSON serializer for Move objects

---
+++ redistricter/src/main/java/giants/redistricter/serialize/MoveSerializer.java
@@ -0,0 +1,26 @@
+package giants.redistricter.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import giants.redistricter.algorithm.Move;
+import giants.redistricter.data.District;
+import giants.redistricter.data.Precinct;
+
+import java.io.IOException;
+
+public class MoveSerializer extends JsonSerializer<Move> {
+
+    @Override
+    public void serialize(Move move, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        Precinct precinct = move.getPrecinct();
+        District srcDistrict = move.getSourceDistrict();
+        District destDistrict = move.getDestinationDistrict();
+
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeNumberField("precinct", precinct.getId());
+        jsonGenerator.writeNumberField("sourceDistrict", srcDistrict.getId());
+        jsonGenerator.writeNumberField("destinationDistrict", destDistrict.getId());
+        jsonGenerator.writeEndObject();
+    }
+}
