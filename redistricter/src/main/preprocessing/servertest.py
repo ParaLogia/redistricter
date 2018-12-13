9b02133287a81f1a6f345d1153ff4ce077172947
Phillip Huang <phillip.huang@stonybrook.edu>
2018-12-12 21:28:09 -0500

    Change servertest script

--- redistricter/src/main/preprocessing/servertest.py
+++ redistricter/src/main/preprocessing/servertest.py
@@ -1,5 +1,7 @@
 import requests
 
+session = requests.Session()
+
 startdata = \
 {
     "state": "NY",
@@ -7,30 +9,31 @@ startdata = \
         "POLSBY_POPPER": 0.3,
         "POPULATION_FAIRNESS": 0.7
     },
-    "algorithm": "REGION_GROWING",
-    "variation": "GREEDY_ACCEPT",
+    "algorithm": "SIMULATED_ANNEALING",
+    "variation": "ANY_ACCEPT",
     "seed": 12356
 }
 
 
 def select(state="NY"):
     data = {"state": state}
-    response = requests.post('http://127.0.0.1:8080/select', data=data)
+    response = session.post('http://127.0.0.1:8080/select', data=data)
     print(response.text)
 
 
 def start(data=startdata):
-    response = requests.post('http://127.0.0.1:8080/start', json=data)
+    response = session.post('http://127.0.0.1:8080/start', json=data)
     print(response.text)
 
 
 def next():
-    response = requests.post('http://127.0.0.1:8080/next')
+    response = session.post('http://127.0.0.1:8080/next')
     print(response.text)
 
 
 def main():
-    select()
+    start()
+    next()
 
 
 if __name__ == "__main__":
