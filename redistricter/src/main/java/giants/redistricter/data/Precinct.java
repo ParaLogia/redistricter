ce171cf2d39f79934cb15fc1508fbe2e6b177190
MatthewArgyropoulos219 <matthew.argyropoulos@stonybrook.edu>
2018-12-13 22:21:39 -0500

    Precincts Update

--- redistricter/src/main/java/giants/redistricter/data/Precinct.java
+++ redistricter/src/main/java/giants/redistricter/data/Precinct.java
@@ -23,115 +23,75 @@ public class Precinct {
 
     @Column(name = "DISTRICT_ID")
     private Integer districtId;
-
-    @Column(name = "GEO_DATA")
-    private String border;
-
-    @Column(name = "CENTER_POINT")
-    private String centerPoint;
+    
     @Column(name = "POPULATION")
     private int population;
-    @Transient
+    @Column(name = "PERIMETER")
     private double perimeter;
+    @Column(name = "AREA")
+    private double area;
 
     @Transient
     private District district;
     @Transient
     private  Map<Precinct,Border> neighbors;
     @Transient
-    private Double area;
-    @Transient
     private Map<Party, Integer> votes;
     @Transient
     private Map<Demographic,Integer> demographics;
-
-
-
-    public Integer getDistrictId() {
-        return districtId;
-    }
-
-    public void setDistrictId(Integer districtId) {
-        this.districtId = districtId;
-    }
-
-    public String getCenterPoint() {
-        return centerPoint;
-    }
-
-    public void setCenterPoint(String centerPoint) {
-        this.centerPoint = centerPoint;
-    }
-
-    public String getBorder() {
-        return border;
-    }
-
-    public void setBorder(String border) {
-        this.border = border;
-    }
-
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
-    public District getDistrict() {
-        return district;
+    public Integer getDistrictId() {
+        return districtId;
     }
-
-    public void setDistrict(District district) {
-        this.district = district;
+    public void setDistrictId(Integer districtId) {
+        this.districtId = districtId;
     }
-
-    public Integer getPopulation() {
+    public int getPopulation() {
         return population;
     }
-
-    public void setPopulation(Integer population) {
+    public void setPopulation(int population) {
         this.population = population;
     }
-
     public double getPerimeter() {
         return perimeter;
     }
-
     public void setPerimeter(double perimeter) {
         this.perimeter = perimeter;
     }
-
+    public double getArea() {
+        return area;
+    }
+    public void setArea(double area) {
+        this.area = area;
+    }
+    public District getDistrict() {
+        return district;
+    }
+    public void setDistrict(District district) {
+        this.district = district;
+    }
     public Map<Precinct, Border> getNeighbors() {
         return neighbors;
     }
-
     public void setNeighbors(Map<Precinct, Border> neighbors) {
         this.neighbors = neighbors;
     }
-
-    public Double getArea() {
-        return area;
-    }
-
-    public void setArea(Double area) {
-        this.area = area;
-    }
-
     public Map<Party, Integer> getVotes() {
         return votes;
     }
-
     public void setVotes(Map<Party, Integer> votes) {
         this.votes = votes;
     }
-
     public Map<Demographic, Integer> getDemographics() {
         return demographics;
     }
-
     public void setDemographics(Map<Demographic, Integer> demographics) {
         this.demographics = demographics;
     }
+
 }
